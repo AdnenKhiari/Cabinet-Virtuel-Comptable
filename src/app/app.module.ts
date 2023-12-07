497197e68468224835260bd03af2e13b72b55a5c
Fares Raboudi <fares.rabboudi@etudiant-fst.utm.tn>
2023-12-07 18:21:22 +0100

    ajout du composant liste-facutures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { ListeFacturesComponent } from './liste-factures/liste-factures.component'
+
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, ListeFacturesComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
