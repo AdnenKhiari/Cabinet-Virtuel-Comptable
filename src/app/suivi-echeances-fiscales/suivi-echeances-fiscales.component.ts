16769a128ac12114e3b79ae3878232775e42523b
Fady Zaafrane <fady.zaafrane@gmail.com>
2023-12-08 20:00:26 +0100

    suivi-echeance-component-updated

--- src/app/suivi-echeances-fiscales/suivi-echeances-fiscales.component.ts
+++ src/app/suivi-echeances-fiscales/suivi-echeances-fiscales.component.ts
@@ -1,21 +1,65 @@
+// suivi-echeances-fiscales.component.ts
+
 import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-suivi-echeances-fiscales',
   templateUrl: './suivi-echeances-fiscales.component.html',
-  styleUrls: ['./suivi-echeances-fiscales.component.css']
+  styleUrls: ['./suivi-echeances-fiscales.component.scss']
 })
 export class SuiviEcheancesFiscalesComponent implements OnInit {
-  echeances!: any[]
+  echeances: any[] = []
+  showWeeks: { [key: string]: boolean } = {}
+  selectedMonth: string | null = null
+  months: string[] = [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre'
+  ]
+  weeks: number[] = [1, 2, 3, 4, 5]
 
   constructor() {}
 
+  toggleWeeks(month: string): void {
+    if (this.selectedMonth === month) {
+      this.selectedMonth = null
+    } else {
+      this.selectedMonth = month
+      this.months.forEach(m => {
+        this.showWeeks[m] = m === month
+      })
+    }
+  }
+
   ngOnInit(): void {
-    // Initialisez les données ici (ex. appel à un service)
+    const currentDate = new Date()
+    const monthIndex = currentDate.getMonth()
+    this.selectedMonth = this.months[monthIndex]
+    this.toggleWeeks(this.selectedMonth)
+
     this.echeances = [
-      { date: '2023-12-31', type: 'Déclaration de TVA' },
-      { date: '2023-12-31', type: 'Déclaration Mensuelle' },
-      { date: '2023-12-31', type: 'Déclaration fiscale' }
+      {
+        date: '2024-01-23',
+        type: 'Déclaration de TVA',
+        description: 'description1',
+        status: 'Traité'
+      },
+      { date: '2023-11-30', type: 'Évènement', description: 'description2', status: 'Non Traité' },
+      {
+        date: '2023-12-31',
+        type: 'Déclaration fiscale',
+        description: 'description3',
+        status: 'Traité'
+      }
     ]
   }
 }
