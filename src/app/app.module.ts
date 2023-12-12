ee396d4d4d9b6a36d34faa4cb448f0c0007b2d48
chaiebsaid.01@gmail.com <chaiebsaid.01@gmail.com>
2023-12-12 19:28:02 +0100

    ajout du compsant gestion user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { ListeDeclarationsFiscalesComponent } from './liste-declarations-fiscales/liste-declarations-fiscales.component'
 import { ReactiveFormsModule } from '@angular/forms'
+import { GestionUserComponent } from './gestion-user/gestion-user.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatTabsModule } from '@angular/material/tabs'
 
 @NgModule({
-  declarations: [AppComponent, ListeDeclarationsFiscalesComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  declarations: [AppComponent, ListeDeclarationsFiscalesComponent, GestionUserComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTabsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
