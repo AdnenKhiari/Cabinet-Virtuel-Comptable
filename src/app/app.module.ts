e946f1ea466e486fefef1050f255ba8e59224760
SEEF <seifeddine.gherir@etudiant-fst.utm.tn>
2023-12-13 15:55:17 +0100

    Created navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { NotificationModule } from './notification/notification.module'
+import { NavbarModule } from './navbar/navbar.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NotificationModule],
+  imports: [BrowserModule, AppRoutingModule, NotificationModule, NavbarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
