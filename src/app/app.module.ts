5e19f5f813dced8076369f5b9fa075cc7c6518a7
Alaa Brahim <alaa.brahim@etudiant-fst.utm.tn>
2023-12-14 13:10:23 +0100

    added Composant de Liste des Clients

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component'
 import { AuthentificationModule } from './authentification/authentification.module'
 import { LoginComponent } from './authentification/login/login.component'
 import { SignUpComponent } from './authentification/sign-up/sign-up.component'
+import { ListeClientsComponent } from './liste-clients/liste-clients.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ListeClientsComponent],
   imports: [BrowserModule, AppRoutingModule, AuthentificationModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/liste-clients/liste-clients.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-liste-clients',
+  templateUrl: './liste-clients.component.html',
+  styleUrls: ['./liste-clients.component.css']
+})
+export class ListeClientsComponent {}
