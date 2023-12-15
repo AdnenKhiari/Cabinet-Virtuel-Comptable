d44a914c0aed51f00a95c178ff53f945b901757b
Mohamed Aziz Idoudi <mohamedaziz.idoudi@etudiant-fst.utm.tn>
2023-12-15 22:16:51 +0100

    auth test for routing

--- src/app/authentification/login/login.component.ts
+++ src/app/authentification/login/login.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, OnInit } from '@angular/core'
 @Component({
   selector: 'app-login',
@@ -5,5 +6,6 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {}
 }
--- src/app/modeles/decfiscales.modeles.ts
+++ src/app/modeles/decfiscales.modeles.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
 export class DecFiscale {
   public firstname: String = ''
   public lastname: String = ''
