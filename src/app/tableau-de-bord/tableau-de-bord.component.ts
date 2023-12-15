43fce6eb4660c40ae7489e27fc6eadc3223a5f9e
Mohamed Aziz Idoudi <mohamedaziz.idoudi@etudiant-fst.utm.tn>
2023-12-15 22:30:57 +0100

    added hyperlinks to routers as well as few changes on the app routing

--- src/app/tableau-de-bord/tableau-de-bord.component.ts
+++ src/app/tableau-de-bord/tableau-de-bord.component.ts
@@ -1,21 +1,19 @@
-import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
-import { Component } from '@angular/core';
-import { CharteChargesComponent } from "../charte-charges/charte-charges.component";
-import { CharteEcartRevenuesChargesComponent } from "../charte-ecart-revenues-charges/charte-ecart-revenues-charges.component";
-import { RevenuesClientsComponent } from "../revenues-clients/revenues-clients.component";
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+import { Component } from '@angular/core'
+import { CharteChargesComponent } from '../charte-charges/charte-charges.component'
+import { CharteEcartRevenuesChargesComponent } from '../charte-ecart-revenues-charges/charte-ecart-revenues-charges.component'
+import { RevenuesClientsComponent } from '../revenues-clients/revenues-clients.component'
 
 @Component({
-    selector: 'app-tableau-de-bord',
-    standalone: true,
-    templateUrl: './tableau-de-bord.component.html',
-    styleUrls: ['./tableau-de-bord.component.css'],
-    imports: [CharteChargesComponent, CharteEcartRevenuesChargesComponent, RevenuesClientsComponent]
+  selector: 'app-tableau-de-bord',
+  standalone: true,
+  templateUrl: './tableau-de-bord.component.html',
+  styleUrls: ['./tableau-de-bord.component.css'],
+  imports: [CharteChargesComponent, CharteEcartRevenuesChargesComponent, RevenuesClientsComponent]
 })
 export class TableauDeBordComponent {
   constructor() {}
-  chiffreAffaire = 100000;
-  Benefice = 10000;
-
-
-  
+  chiffreAffaire = 100000
+  Benefice = 10000
 }
