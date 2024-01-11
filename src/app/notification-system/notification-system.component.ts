5516455ab16f816234f8fbc6341ae552ed27b439
hamoud <chaarimahmoud55@gmail.com>
2024-01-11 21:44:28 +0100

    11/01/2024

---
+++ src/app/notification-system/notification-system.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-system',
+  standalone: true,
+  imports: [],
+  templateUrl: './notification-system.component.html',
+  styleUrl: './notification-system.component.css'
+})
+export class NotificationSystemComponent {
+
+}
