7d24c623e7c338d4441cf178f165a124c6afa40b
eyabouhouch <129467482+eyabouhouch@users.noreply.github.com>
2024-01-12 20:32:24 +0100

    Add files via upload

---
+++ forgot-password/forgot-password.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent {
+  forgotPasswordForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.forgotPasswordForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {
+    if (this.forgotPasswordForm.valid) {
+      // Effectuez les actions nécessaires pour envoyer l'e-mail de récupération
+      console.log('Form submitted:', this.forgotPasswordForm.value);
+    }
+  }
+}
