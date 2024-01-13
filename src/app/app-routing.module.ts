1a02141110cdf6c3004c80c2515115019ca8e72d
malek-jellali <malekjellali29@gmail.com>
2024-01-13 12:00:33 +0100

    login typescript code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,11 @@ import { LoginComponent } from './authentification/login/login.component'
 import { SignUpComponent } from './authentification/sign-up/sign-up.component'
 
 const routes: Routes = [
-  {path:"",component:LoginComponent},
-  {path:"login",component:LoginComponent},
-  {path:"forget-password",component:ForgotPasswordComponent},
-  {path:"sign-up",component:SignUpComponent},
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'forget-password', component: ForgotPasswordComponent },
+  { path: 'sign-up', component: SignUpComponent }
   // {path:"**",component:PageNotFoundComponent}
-
 ]
 
 @NgModule({
--- src/app/authentification/login/login.component.ts
+++ src/app/authentification/login/login.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  ngOnInit() {}
+  userFormGroup!: FormGroup
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.userFormGroup = this.fb.group({
+      email: this.fb.control(''),
+      password: this.fb.control('')
+    })
+  }
+
+  handleLogin(): void {
+    // Implement your login logic here
+    let email = this.userFormGroup.value.email
+    let password = this.userFormGroup.value.password
+  }
 }
