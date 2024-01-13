9850bec84bb2ea07fe6f02a85886434c8b5561ec
meriam <merriambelhadjhamida@gmail.com>
2024-01-13 05:00:18 +0100

    injected the authorisation service

--- src/app/tableau-de-bord/tableau-de-bord.component.ts
+++ src/app/tableau-de-bord/tableau-de-bord.component.ts
@@ -4,16 +4,19 @@ import { Component } from '@angular/core'
 import { CharteChargesComponent } from '../charte-charges/charte-charges.component'
 import { CharteEcartRevenuesChargesComponent } from '../charte-ecart-revenues-charges/charte-ecart-revenues-charges.component'
 import { RevenuesClientsComponent } from '../revenues-clients/revenues-clients.component'
+import { AuthorizationService } from '../autorisation/authorization.service'
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-tableau-de-bord',
   standalone: true,
   templateUrl: './tableau-de-bord.component.html',
   styleUrls: ['./tableau-de-bord.component.css'],
-  imports: [CharteChargesComponent, CharteEcartRevenuesChargesComponent, RevenuesClientsComponent]
+  imports: [CharteChargesComponent, CharteEcartRevenuesChargesComponent, RevenuesClientsComponent,CommonModule]
 })
 export class TableauDeBordComponent {
-  constructor() {}
+
+  constructor(public authService :AuthorizationService) {}
   chiffreAffaire = 100000
   Benefice = 10000
 }
