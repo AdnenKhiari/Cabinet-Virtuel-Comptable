93cd38588c405173f4703673dc583f171d6f13b5
meriam <merriambelhadjhamida@gmail.com>
2024-01-13 19:22:18 +0100

    added another condition  to guards

--- src/app/guards/autorisation.guard.ts
+++ src/app/guards/autorisation.guard.ts
@@ -2,17 +2,27 @@ import {Injectable} from '@angular/core'
 import { ActivatedRouteSnapshot,CanActivate,Router,RouterStateSnapshot,UrlTree } from '@angular/router'
 import { Observable } from 'rxjs'
 import { AuthorizationService } from '../autorisation/authorization.service';
+import { AuthService } from '../authentification/auth.service';
 
 @Injectable({
   providedIn: 'root'
   })
 export class autorisationGuard implements CanActivate{
-  constructor(private authorizationService:AuthorizationService,private router:Router){
+  constructor(private authorizationService:AuthorizationService,private router:Router,private authentificationService:AuthService){
 
   }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-   const requiredRole=route.data['requiredRole'] as string;
-   if(this.authorizationService.hasRole(requiredRole)){
+    if (!this.authentificationService.isUserLoggedIn()) {
+    
+      this.router.navigate(['/Login']);
+      return false;
+    }
+
+   
+   
+    const requiredRole=route.data['requiredRole'] as string[];
+
+   if(requiredRole.some((role: string) => this.authorizationService.hasRole(role))){
     return true;
    }
    else{
