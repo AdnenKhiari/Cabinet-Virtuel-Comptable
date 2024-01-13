23f7b8b0c33b2ce8e70a47da1dad50a6bdb224f0
Adnen Kh <86753901+AdnenKhiari@users.noreply.github.com>
2024-01-13 22:56:49 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HttpClientModule } from '@angular/common/http'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts'
 import { AuthentificationModule } from './authentification/authentification.module'
