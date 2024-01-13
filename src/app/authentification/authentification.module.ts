46a46badc29dd102449888d0c396954709a943ac
meriam <merriambelhadjhamida@gmail.com>
2024-01-13 19:35:33 +0100

    added some imports

--- src/app/authentification/authentification.module.ts
+++ src/app/authentification/authentification.module.ts
@@ -5,10 +5,13 @@ import { Router, RouterModule } from '@angular/router'
 import { LoginComponent } from './login/login.component'
 import { SignUpComponent } from './sign-up/sign-up.component'
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component'
+import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [LoginComponent, SignUpComponent, ForgotPasswordComponent],
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,ReactiveFormsModule,HttpClientModule],
   exports: [LoginComponent, SignUpComponent, ForgotPasswordComponent]
 })
 export class AuthentificationModule {}
