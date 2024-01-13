1054ab8e8755a39c527d00efc220c8d72bda21f4
Fakhfakh Iheb <ihebfakhfakh123@gmail.com>
2024-01-13 17:18:00 +0100

    notification iheb

--- src/app/notification_iheb/notification-system.component.ts
+++ src/app/notification_iheb/notification-system.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-notification-system',
-  standalone: true,
-  imports: [],
   templateUrl: './notification-system.component.html',
-  styleUrl: './notification-system.component.css'
+  styleUrls: ['./notification-system.component.css']
 })
 export class NotificationSystemComponent {}
