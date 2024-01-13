02aabb49453bb62de3863fad458f1f96a57bb683
jouini-wajih <jouiniwajih2001@gmail.com>
2024-01-13 20:46:38 +0100

    NgRx packages installing with npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'
+import { NgModule, isDevMode } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
@@ -30,7 +30,10 @@ import { MatTabsModule } from '@angular/material/tabs'
 import { ConfigurationComponent } from './configuration/configuration.component'
 import { ListeClientsComponent } from './liste-clients/liste-clients.component'
 import { FormsModule } from '@angular/forms'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 
 @NgModule({
   declarations: [
@@ -62,7 +65,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
     TableauDeBordComponent,
     ImpressionsDocumentsListComponent,
     FormsModule,
-    NgbModule
+    NgbModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ]
 })
 export class AppModule {}
