232b6da45c1262140362b6059879de2de3f210f1
meriam <merriambelhadjhamida@gmail.com>
2024-01-13 18:24:44 +0100

    added group form to forgot-password

--- src/app/authentification/forgot-password/forgot-password.component.ts
+++ src/app/authentification/forgot-password/forgot-password.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core'
+import { Component,OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
-export class ForgotPasswordComponent {}
+export class ForgotPasswordComponent  implements OnInit{
+resetForm!:FormGroup 
+constructor(private fb:FormBuilder){
+  
+}
+ngOnInit():void{
+  this.resetForm=this.fb.group({
+    email:['',[Validators.required,Validators.email]]
+  })
+}
+sendResetLink() {
+console.log(this.resetForm?.value);
+}
+}
