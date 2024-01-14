808741f5be2f9c8f0cc360faad7c49f003c45b02
AdnenKhiari <adnenkhiari484@gmail.com>
2024-01-14 12:22:58 +0100

    solved url import build error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,75 +5,72 @@ import { RouterModule, Routes } from '@angular/router'
 import { ForgotPasswordComponent } from './authentification/forgot-password/forgot-password.component'
 import { LoginComponent } from './authentification/login/login.component'
 import { SignUpComponent } from './authentification/sign-up/sign-up.component'
-
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'forget-password', component: ForgotPasswordComponent },
-  { path: 'sign-up', component: SignUpComponent },
-  // {path:"**",component:PageNotFoundComponent}
-  {
-    path: 'historique',
-    loadChildren: () =>
-      import('./historique-audit/historique-audit.module').then(m => m.HistoriqueAuditModule)
-  },
   {
     path: '',
     component: MainOutletComponent,
     children: [
+      { path: '', component: LoginComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'forget-password', component: ForgotPasswordComponent },
+      { path: 'sign-up', component: SignUpComponent },
+      // {path:"**",component:PageNotFoundComponent}
       {
-        path: '',
-        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-      }
-    ]
-  },
-  {
-    path: 'auth',
-    component: MainOutletComponent,
-    children: [
-      {
-        path: '',
-        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-      }
-    ]
-  },
-  {
-    path: 'client',
-    component: MainOutletComponent,
-    children: [
-      {
-        path: '',
-        loadChildren: () => import('./users/client/client.module').then(m => m.ClientModule)
-      }
-    ]
-  },
-  {
-    path: 'admin',
-    component: MainOutletComponent,
-    children: [
-      {
-        path: '',
-        loadChildren: () => import('./users/admin/admin.module').then(m => m.AdminModule)
-      }
-    ]
-  },
-  {
-    path: 'comptable',
-    component: MainOutletComponent,
-    children: [
-      {
-        path: '',
+        path: 'historique',
         loadChildren: () =>
-          import('./users/comptable/comptable.module').then(m => m.ComptableModule)
-      }
-    ]
-  },
-  {
-    path: 'senior',
-    component: MainOutletComponent,
-    children: [
+          import('./historique-audit/historique-audit.module').then(m => m.HistoriqueAuditModule)
+      },
+
       {
-        path: '',
-        loadChildren: () => import('./users/senior/senior.module').then(m => m.SeniorModule)
+        path: 'auth',
+        component: MainOutletComponent,
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+          }
+        ]
+      },
+      {
+        path: 'client',
+        component: MainOutletComponent,
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./users/client/client.module').then(m => m.ClientModule)
+          }
+        ]
+      },
+      {
+        path: 'admin',
+        component: MainOutletComponent,
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./users/admin/admin.module').then(m => m.AdminModule)
+          }
+        ]
+      },
+      {
+        path: 'comptable',
+        component: MainOutletComponent,
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('./users/comptable/comptable.module').then(m => m.ComptableModule)
+          }
+        ]
+      },
+      {
+        path: 'senior',
+        component: MainOutletComponent,
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./users/senior/senior.module').then(m => m.SeniorModule)
+          }
+        ]
       }
     ]
   }
